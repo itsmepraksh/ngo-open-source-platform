c39f1ed6feadde777a7ca5c1c490d6fe053f4473
Prakash Samanta <druvils123@gmail.com>
2025-08-29 16:58:50 +0530

    Feat: (29-08-25) fix button bug in event_section form opening

--- frontend/src/layouts/Admin_Events_Section.jsx
+++ frontend/src/layouts/Admin_Events_Section.jsx
@@ -7,11 +7,12 @@ const Admin_Events_Section = () => {
     <div id="events" className="content-section">
       <div className="section-header">
         <h3>Events & Activities Management</h3>
-        <button className="btn btn-success" onclick="addNewEvent()">
+       
           <Link to="/events_section/form">
+           <button className="btn btn-success" onclick="addNewEvent()">
             <i className="fas fa-plus"></i> Add New Event
-          </Link>
-        </button>
+        </button> 
+        </Link>
       </div>
       <div className="section-content">
         <div id="events-list">{/* <!-- Events will be loaded here --> */}</div>
