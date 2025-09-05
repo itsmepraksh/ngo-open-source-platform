b43e1dde9ecf4a2308dc246d4721c858447b6543
Prakash Samanta <druvils123@gmail.com>
2025-09-05 06:37:32 +0530

    Feat: (05-09-25) fixed navbar bug

--- frontend/src/components/Navbar/Nav.jsx
+++ frontend/src/components/Navbar/Nav.jsx
@@ -1,29 +1,87 @@
 import { Link } from "react-router-dom";
-
-import style from "../Navbar/nav.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import style from "../Navbar/nav.module.css";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Nav = () => {
-    
+  const [isClicked, setIsClicked] = useState(false);
+  console.log(isClicked);
   return (
     <nav className={`${style.navbar}`}>
-        <div className={`${style.nav_container}`}>
-            <Link href="#home" className={`${style.logo}`} id="logoText">Lavanya Foundation</Link>
-            <ul className={`${style.nav_links}`}>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/events">Events</Link></li>
-                <li><Link to="/volunteer">Volunteer</Link></li>
-                <li><Link to="/donatex">Donate</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/shop">Shop</Link></li>
-            </ul>
-            <button className={`${style.mobile_menu}`}>
-                <i className="fas fa-bars"></i>
-            </button>
-        </div>
+      <div className={`${style.nav_container}`}>
+        <Link href="#home" className={`${style.logo}`} id="logoText">
+          Lavanya Foundation
+        </Link>
+        <ul className={ `${style.nav_links2}` }>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/events">Events</Link>
+        </li>
+        <li>
+          <Link to="/volunteer">Volunteer</Link>
+        </li>
+        <li>
+          <Link to="/donatex">Donate</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+      </ul>
+        <button
+          onClick={() => setIsClicked(!isClicked)}
+          className={`${style.mobile_menu}`}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+      <ul className={isClicked ? `${style.nav_links}` : "hidden"}>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/home">Home</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/about">About</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/events">Events</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/volunteer">Volunteer</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/donatex">Donate</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link 
+          onClick={() => setIsClicked(!isClicked)} to="/shop">Shop</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
